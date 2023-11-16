6f0cc5ab851ecd0f68fcb72cebffce22e62ad466
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-15 16:23:39 -0800

    Add function so users can keep scrolling through stolen bikes
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
 import BikeService from './service';
 
+// Function to display stolen bike details
 function displayStolen(i, response) {
-  // sets bike image
-  document.querySelector("img#bikeImage").src = response.bikes[i].large_img;
+  const bikesCount = response.bikes.length;
+  console.log(response);
+  if (bikesCount === 0) {
+    // Handle the case when there are no stolen bikes
+    document.querySelector("img#bikeImage").src = 'placeholder-image.jpg';
+    document.querySelector("span#dateStolen").innerText = 'No stolen bikes found.';
+    return;
+  }
+  // Calculate the index considering the loop
+  i = i % bikesCount;
 
-  // sets stolen date
+  // Set bike image
+  if(response.bikes[i].large_img === null) {
+    document.querySelector("img#bikeImage").src = "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
+  } else {
+    document.querySelector("img#bikeImage").src = response.bikes[i].large_img;
+  }
+
+  // Set stolen date
   const timestamp = response.bikes[i].date_stolen;
   const date = new Date(timestamp * 1000);
   const formattedDate = date.toLocaleString();
@@ -18,14 +35,15 @@ function printStolenBike(response) {
   let i = 0;
   displayStolen(i, response);
 
-  // increments through stolen bikes from next button
+  // "Next" button displays next stolen bike
   document.querySelector("button#nextStolen").addEventListener("click", (e) => {
     e.preventDefault();
-    i++;
+    i++; // Increment index
     displayStolen(i, response);
   });
 }
 
+// Function to print bike statistics
 function printBikeStats(response) {
   const bikeStats = document.querySelector("div#bikeStats");
   bikeStats.innerHTML = '';
@@ -36,30 +54,36 @@ function printBikeStats(response) {
   bikeStats.innerHTML += "Stolen: " + response.bike.stolen + "<br>";
   bikeStats.innerHTML += "Ownership: " + response.bike.status + "<br>";
 }
-
 async function getBikeDetails(id) {
+  // Call the BikeService to get bike details
   const response = await BikeService.getBikeDetails(id);
-  if(!response) {
+
+  if (!response) {
     window.alert("Error");
   } else {
     printBikeStats(response);
   }
 }
+
 async function getStolenBike(location) {
+  // Call the BikeService to get stolen bike details
   const response = await BikeService.getStolenBike(location);
-  if(!response) {
+
+  if (!response) {
     window.alert("Error");
   } else {
     printStolenBike(response);
   }
 }
 
+// Event handler for Bike Search form
 function handleBikeSearch(e) {
   e.preventDefault();
   let id = document.querySelector("input#bikeID").value;
   getBikeDetails(id);
 }
 
+// Event handler for Stolen Bike form
 function handleStolenBike(e) {
   e.preventDefault();
   let location = document.querySelector("input#stolenArea").value;
@@ -67,15 +91,11 @@ function handleStolenBike(e) {
 }
 
 window.addEventListener("load", () => {
+  // Select forms for bike and stolen bike searches
   const formBikeStats = document.querySelector("form#bikeSearch");
   const formStolenBikes = document.querySelector("form#stolenSearch");
+
+  // Add event listeners for form submissions
   formBikeStats.addEventListener("submit", handleBikeSearch);
   formStolenBikes.addEventListener("submit", handleStolenBike);
 });
-//   BikeService.getStolenBike(id);
-// .then((response) => {
-//   displayBikeInfo(response);
-// })
-// .catch((error) => {
-//   console.error(error);
-// });
