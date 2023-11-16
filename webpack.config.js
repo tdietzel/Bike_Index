c6adf3594feb860bd6a2bf84378361c830fabcc1
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-15 16:25:57 -0800

    Update webpack.config.js title

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Shape Tracker",
+      title: "Bike Index",
       template: "./src/index.html",
       inject: "body",
     }),
